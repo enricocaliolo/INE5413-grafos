977b607e914e2532f620f225e61a1b96cf96c878
enrico <enricocalioloo@gmail.com>
2024-04-16 15:21:09 -0300

    bellman-ford funcionando, a princípio

--- A1/lib/graph.py
+++ A1/lib/graph.py
@@ -10,21 +10,28 @@ class Vertice:
     def __str__(self) -> str:
         return f"({self.rotulo})"
 
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, self.__class__):
+            if self.index == other.index:
+                return True
+
+        return False
+
     def add_vizinhos(self, vizinho, peso=0):
         self.vizinhos[vizinho] = peso
 
 
-# class Aresta:
-#     def __init__(self, vertice1, vertice2, peso=0) -> None:
-#         self.vertice1 = vertice1
-#         self.vertice2 = vertice2
-#         self.peso = peso
+class Aresta:
+    def __init__(self, u: int, v: int, peso=0) -> None:
+        self.u = u
+        self.v = v
+        self.peso = peso
 
 
 class Graph:
     def __init__(self, path) -> None:
         self.vertices: dict[int, Vertice] = {}
-        self.arestas: dict[tuple[int, int]] = {}
+        self.arestas: dict[tuple[int, int], Aresta] = {}
         self.n_vertices = 0
         self.n_arestas = 0
 
@@ -52,11 +59,13 @@ class Graph:
                 elif edges:
                     edges = line.split(" ")
 
-                    v = int(edges[0])
-                    u = int(edges[1])
+                    u = int(edges[0])
+                    v = int(edges[1])
                     peso = float(edges[2])
 
-                    self.arestas[(v, u)] = peso
+                    aresta = Aresta(u, v, peso)
+
+                    self.arestas[(u, v)] = aresta
 
                     self.vertices[u].add_vizinhos(v, peso)
                     self.vertices[v].add_vizinhos(u, peso)
@@ -87,7 +96,28 @@ class Graph:
         return self.vertices[v].vizinhos
 
     def haAresta(self, u, v):
-        return (u, v) in self.arestas
+        if (u, v) in self.arestas:
+            return True
+        return False
 
     def peso(self, u, v):
         return self.arestas[(u, v)].peso if (u, v) in self.arestas else float("inf")
+
+    def findAresta(self, v: int, u: int) -> Aresta:
+        if (v, u) in self.arestas:
+            return self.arestas(v, u)
+        elif (u, v) in self.arestas:
+            return self.arestas(u, v)
+
+        return None
+
+    def findVertice(self, v: int):
+        return self.vertices[v] if v in self.vertices else False
+
+    def traverseArestas(self):
+        for a in self.arestas.values():
+            yield a
+
+    def traverseVertices(self):
+        for v in self.vertices.values():
+            yield v

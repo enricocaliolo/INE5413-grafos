9591ed4fddf9c86639fe83f20e5d293586035ccc
enrico <enricocalioloo@gmail.com>
2024-04-27 14:23:09 -0300

    floyd warshall

---
+++ A1/floyd_warshall.py
@@ -0,0 +1,35 @@
+from graph import Graph
+
+
+def floyd_warshall(graph: Graph):
+
+    n_vertices: int = graph.n_vertices
+
+    matriz = [[0] * n_vertices for i in range(n_vertices)]
+
+    for i in range(n_vertices):
+        for j in range(n_vertices):
+            if i == j:
+                matriz[i][j] = 0
+
+            elif not graph.findAresta(i, j):
+                matriz[i][j] = float("inf")
+                matriz[j][i] = float("inf")
+
+            else:
+                aresta = graph.findAresta(i, j)
+                matriz[i][j] = graph.arestas[aresta].peso
+
+    for k in range(n_vertices):
+        for j in range(n_vertices):
+            for i in range(n_vertices):
+                matriz[j][i] = min(matriz[j][i], matriz[j][k] + matriz[k][i])
+
+    for i in range(n_vertices):
+        print(f"{i+1}: {','.join(str(e) for e in matriz[i])}")
+
+
+if __name__ == "__main__":
+
+    graph = Graph("arquivo_menor_teste.net")
+    floyd_warshall(graph)
